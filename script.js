d4ccdda3419f98c14a6537504b50bd9ba6b1c035
mitrandir666 <a.t.szypryt@gmail.com>
2024-05-22 20:41:07 +0200

    creating background-color changing function with adjustable opacit for each grid cell

--- script.js
+++ script.js
@@ -13,10 +13,15 @@ setGrid();
 makeGrid();
 
 
-function setGrid(){
-    sliderValue.innerText = slider.value;
-    gridResolution = slider.value;
-    makeGrid();
+//tutaj sprobuje zmianiac kolor tla na razie bez zadnej funkcji
+
+//znalazlem "hasPointerCapture() method w mdm" oraz "pointerCapture"
+
+
+function setGrid(){//funkcja ustawia i wyswietla wartosc suwaka oraz aktualizuje wartosc zmiennej gridResolution
+    sliderValue.innerText = slider.value;//tekst obok suwaka 8-100
+    gridResolution = slider.value;// aktualizuje wartosc zmiennej przechowujacej wymiary siatki
+    makeGrid();// nastepnie na podstawie wartosci zmiennej gridResolution tworzy nowa siatke
 }
 
 
@@ -37,6 +42,15 @@ function makeGrid(){
             singleCell.style.width = (containerSize / gridResolution) - 1 + "px";
             singleCell.style.borderLeft = "1px solid black";
             gridRow.appendChild(singleCell);
+
+            //dodanie zmiany koloru tla po najechaniu kurosorem myszy na czarny
+            let opacity = 0.1;
+            singleCell.addEventListener('mouseenter', function(){
+                opacity+=0.1;
+                opacity = Math.min(1, opacity);//ograniczenie opacity do 1
+                singleCell.style.backgroundColor = `rgba(0,0,0,${opacity})`;
+                console.log(opacity);
+            });
         };
     };
 };
