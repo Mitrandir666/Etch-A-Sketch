1485d5d23c5881857119649fa702f8ec90cbcd4e
mitrandir666 <a.t.szypryt@gmail.com>
2024-05-23 18:41:08 +0200

    creating button with drawing functions - black, oapcity, rainbow

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ const gridField = document.querySelector("#gridField");
 const settingsField = document.querySelector("#settingsField");
 const slider = document.querySelector('#slider');
 const sliderValue = document.querySelector('#sliderValue');
+const blackColorButton = document.querySelector('#blackColorButton');
+const opacityBlackColorButton = document.querySelector('#opacityBlackColorButton');
+const rainbowColorButton = document.querySelector('#rainbowColorButton');
+
 
 
 slider.addEventListener('input', setGrid);
@@ -12,10 +16,6 @@ slider.addEventListener('input', setGrid);
 setGrid();
 makeGrid();
 
-
-//tutaj sprobuje zmianiac kolor tla na razie bez zadnej funkcji
-
-//znalazlem "hasPointerCapture() method w mdm" oraz "pointerCapture"
 
 
 function setGrid(){//funkcja ustawia i wyswietla wartosc suwaka oraz aktualizuje wartosc zmiennej gridResolution
@@ -30,27 +30,46 @@ function makeGrid(){
     for (let i=1; i<= gridResolution; i++){// tworzy poziome kontenery dla pojedynczych komorek
         let gridRow = document.createElement("div");
         gridRow.classList.add("gridRow");
-        gridRow.style.height = (containerSize / gridResolution)-1 + "px";
-        gridRow.style.borderBottom = "1px solid black";
+        gridRow.style.height = (containerSize / gridResolution) + "px";
+        
         gridRow.style.display = "flex";
         gridField.appendChild(gridRow);
 
         for (let j=1; j<=gridResolution; j++){// tworzy pojedyncze komorki w poziomych kontenerach
             let singleCell = document.createElement("div");
             singleCell.classList.add("singleCell");
-            singleCell.style.height = (containerSize / gridResolution) - 1 + "px";
-            singleCell.style.width = (containerSize / gridResolution) - 1 + "px";
-            singleCell.style.borderLeft = "1px solid black";
+            singleCell.style.height = (containerSize / gridResolution) + "px";
+            singleCell.style.width = (containerSize / gridResolution) + "px";
+            
             gridRow.appendChild(singleCell);
 
-            //dodanie zmiany koloru tla po najechaniu kurosorem myszy na czarny
-            let opacity = 0.1;
-            singleCell.addEventListener('mouseenter', function(){
-                opacity+=0.1;
-                opacity = Math.min(1, opacity);//ograniczenie opacity do 1
-                singleCell.style.backgroundColor = `rgba(0,0,0,${opacity})`;
-                console.log(opacity);
-            });
+                //rysowanie tylko zwyklego czarnego tracka:
+                blackColorButton.addEventListener('click', function(){
+                    singleCell.addEventListener('mouseenter', function(){
+                        singleCell.style.backgroundColor = "black";
+                    });
+                });
+
+                //rysowanie czarnego tracka z opacity
+                opacityBlackColorButton.addEventListener('click', function(){
+                  let opacity = 0.0;
+                    singleCell.addEventListener('mouseenter', function(){
+                        opacity+=0.1;
+                        opacity = Math.min(1, opacity);//ograniczenie opacity do 1
+                        singleCell.style.backgroundColor = `rgba(0,0,0,${opacity})`;
+                    });
+                });
+
+                //rysowanie losowym kolorem
+                rainbowColorButton.addEventListener('click', function(){
+                    singleCell.addEventListener('mouseenter', function(){
+                        let red = Math.floor(Math.random()*256);
+                        let green = Math.floor(Math.random()*256);
+                        let blue = Math.floor(Math.random()*256);
+                        singleCell.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+                    });
+                });
+                
         };
     };
 };
